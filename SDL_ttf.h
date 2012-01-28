0133d811db7de546a7f629215c7a447f800fbd2d
Sam Lantinga <slouken@libsdl.org>
2012-01-28 11:16:02 -0500

    Updated to version 2.0.12

--- SDL_ttf.h
+++ SDL_ttf.h
@@ -39,7 +39,7 @@ extern "C" {
 */
 #define SDL_TTF_MAJOR_VERSION	2
 #define SDL_TTF_MINOR_VERSION	0
-#define SDL_TTF_PATCHLEVEL	11
+#define SDL_TTF_PATCHLEVEL	12
 
 /* This macro can be used to fill a version structure with the compile-time
  * version of the SDL_ttf library.
