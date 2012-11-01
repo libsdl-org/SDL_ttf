88aaecec453b5cabfdca9138932f9da5bd3967fe
Sam Lantinga <slouken@libsdl.org>
2012-10-31 21:43:56 -0700

    Compile FreeType into SDL_ttf on Android, to simplify build process

--- Android.mk
+++ Android.mk
@@ -4,14 +4,38 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL2_ttf
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(LOCAL_PATH)/../freetype/include
+FREETYPE := $(LOCAL_PATH)/freetype
 
-LOCAL_SRC_FILES := SDL_ttf.c
+LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(FREETYPE)/include
+
+LOCAL_SRC_FILES := SDL_ttf.c \
+		freetype/src/autofit/autofit.c   \
+		freetype/src/base/ftbase.c       \
+		freetype/src/base/ftbitmap.c     \
+		freetype/src/base/ftglyph.c      \
+		freetype/src/base/ftinit.c       \
+		freetype/src/base/ftstroke.c     \
+		freetype/src/base/ftsystem.c	 \
+		freetype/src/bdf/bdf.c           \
+		freetype/src/cff/cff.c           \
+		freetype/src/cid/type1cid.c      \
+		freetype/src/gzip/ftgzip.c       \
+		freetype/src/lzw/ftlzw.c         \
+		freetype/src/pcf/pcf.c           \
+		freetype/src/pfr/pfr.c           \
+		freetype/src/psaux/psaux.c       \
+		freetype/src/pshinter/pshinter.c \
+		freetype/src/psnames/psmodule.c  \
+		freetype/src/raster/raster.c     \
+		freetype/src/sfnt/sfnt.c         \
+		freetype/src/smooth/smooth.c     \
+		freetype/src/truetype/truetype.c \
+		freetype/src/type1/type1.c       \
+		freetype/src/type42/type42.c     \
+		freetype/src/winfonts/winfnt.c   \
+
+LOCAL_CFLAGS += -DFT2_BUILD_LIBRARY
 
 LOCAL_SHARED_LIBRARIES := SDL2
-
-LOCAL_STATIC_LIBRARIES := freetype
-
-LOCAL_LDLIBS := -lz
 
 include $(BUILD_SHARED_LIBRARY)
