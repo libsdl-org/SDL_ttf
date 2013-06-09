4aed91e2981237ebdbe483e27b53316ec8348071
Sam Lantinga <slouken@libsdl.org>
2013-06-09 11:12:46 -0700

    Updated to the latest version of FreeType and included it in the external directory for easy building.

--- Android.mk
+++ Android.mk
@@ -4,37 +4,60 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := SDL2_ttf
 
-FREETYPE := $(LOCAL_PATH)/freetype
+FREETYPE_LIBRARY_PATH := external/freetype-2.4.12
 
-LOCAL_CFLAGS := -I$(LOCAL_PATH)/../SDL/include -I$(FREETYPE)/include
+LOCAL_C_INCLUDES := $(NDK_PROJECT_PATH)/jni/SDL/include
 
 LOCAL_SRC_FILES := SDL_ttf.c \
-		freetype/src/autofit/autofit.c   \
-		freetype/src/base/ftbase.c       \
-		freetype/src/base/ftbitmap.c     \
-		freetype/src/base/ftglyph.c      \
-		freetype/src/base/ftinit.c       \
-		freetype/src/base/ftstroke.c     \
-		freetype/src/base/ftsystem.c	 \
-		freetype/src/bdf/bdf.c           \
-		freetype/src/cff/cff.c           \
-		freetype/src/cid/type1cid.c      \
-		freetype/src/gzip/ftgzip.c       \
-		freetype/src/lzw/ftlzw.c         \
-		freetype/src/pcf/pcf.c           \
-		freetype/src/pfr/pfr.c           \
-		freetype/src/psaux/psaux.c       \
-		freetype/src/pshinter/pshinter.c \
-		freetype/src/psnames/psmodule.c  \
-		freetype/src/raster/raster.c     \
-		freetype/src/sfnt/sfnt.c         \
-		freetype/src/smooth/smooth.c     \
-		freetype/src/truetype/truetype.c \
-		freetype/src/type1/type1.c       \
-		freetype/src/type42/type42.c     \
-		freetype/src/winfonts/winfnt.c   \
 
-LOCAL_CFLAGS += -DFT2_BUILD_LIBRARY
+ifneq ($(FREETYPE_LIBRARY_PATH),)
+    LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(FREETYPE_LIBRARY_PATH)/include
+    LOCAL_CFLAGS += -DFT2_BUILD_LIBRARY
+    LOCAL_SRC_FILES += \
+        $(FREETYPE_LIBRARY_PATH)/src/autofit/autofit.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftbase.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftbbox.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftbdf.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftbitmap.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftcid.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftdebug.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftfstype.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftgasp.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftglyph.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftgxval.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftinit.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftlcdfil.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftmm.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftotval.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftpatent.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftpfr.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftstroke.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftsynth.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftsystem.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/fttype1.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftwinfnt.c \
+        $(FREETYPE_LIBRARY_PATH)/src/base/ftxf86.c \
+        $(FREETYPE_LIBRARY_PATH)/src/bdf/bdf.c \
+        $(FREETYPE_LIBRARY_PATH)/src/bzip2/ftbzip2.c \
+        $(FREETYPE_LIBRARY_PATH)/src/cache/ftcache.c \
+        $(FREETYPE_LIBRARY_PATH)/src/cff/cff.c \
+        $(FREETYPE_LIBRARY_PATH)/src/cid/type1cid.c \
+        $(FREETYPE_LIBRARY_PATH)/src/gzip/ftgzip.c \
+        $(FREETYPE_LIBRARY_PATH)/src/lzw/ftlzw.c \
+        $(FREETYPE_LIBRARY_PATH)/src/pcf/pcf.c \
+        $(FREETYPE_LIBRARY_PATH)/src/pfr/pfr.c \
+        $(FREETYPE_LIBRARY_PATH)/src/psaux/psaux.c \
+        $(FREETYPE_LIBRARY_PATH)/src/pshinter/pshinter.c \
+        $(FREETYPE_LIBRARY_PATH)/src/psnames/psmodule.c \
+        $(FREETYPE_LIBRARY_PATH)/src/raster/raster.c \
+        $(FREETYPE_LIBRARY_PATH)/src/sfnt/sfnt.c \
+        $(FREETYPE_LIBRARY_PATH)/src/smooth/smooth.c \
+        $(FREETYPE_LIBRARY_PATH)/src/tools/apinames.c \
+        $(FREETYPE_LIBRARY_PATH)/src/truetype/truetype.c \
+        $(FREETYPE_LIBRARY_PATH)/src/type1/type1.c \
+        $(FREETYPE_LIBRARY_PATH)/src/type42/type42.c \
+        $(FREETYPE_LIBRARY_PATH)/src/winfonts/winfnt.c
+endif
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
