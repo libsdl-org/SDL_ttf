5f165131c41c67aa31f3ae425564e23838ab1106
Sylvain Becker <sylvain.becker@gmail.com>
2019-03-25 14:47:35 +0100

    Add extend API for text measurement (bug 4515) and improve *_Wrapped functions
    Bug 4515:
      - add an extend API for text measurement without rendering
      e.g: functions named TTF_Measure*()
    
    *_Wrapped functions:
      - text is measured to foresee the wrapping, it avoids to have too many
        calls to Size() functions
      - 'words' that wouldn't fit within the wrap length are now cut.

--- SDL_ttf.h
+++ SDL_ttf.h
@@ -169,12 +169,25 @@ extern DECLSPEC int SDLCALL TTF_GlyphIsProvided(TTF_Font *font, Uint16 ch);
  */
 extern DECLSPEC int SDLCALL TTF_GlyphMetrics(TTF_Font *font, Uint16 ch,
                      int *minx, int *maxx,
-                                     int *miny, int *maxy, int *advance);
+                     int *miny, int *maxy, int *advance);
 
 /* Get the dimensions of a rendered string of text */
 extern DECLSPEC int SDLCALL TTF_SizeText(TTF_Font *font, const char *text, int *w, int *h);
 extern DECLSPEC int SDLCALL TTF_SizeUTF8(TTF_Font *font, const char *text, int *w, int *h);
 extern DECLSPEC int SDLCALL TTF_SizeUNICODE(TTF_Font *font, const Uint16 *text, int *w, int *h);
+
+/* Get the measurement string of text without rendering
+   e.g. the number of characters that can be rendered before reaching 'measure_width'
+
+   in:
+   measure_width - in pixels to measure this text
+   out:
+   count  - number of characters that can be rendered
+   extent - latest calculated width
+*/
+extern DECLSPEC int SDLCALL TTF_MeasureText(TTF_Font *font, const char *text, int measure_width, int *extent, int *count);
+extern DECLSPEC int SDLCALL TTF_MeasureUTF8(TTF_Font *font, const char *text, int measure_width, int *extent, int *count);
+extern DECLSPEC int SDLCALL TTF_MeasureUNICODE(TTF_Font *font, const Uint16 *text, int measure_width, int *extent, int *count);
 
 /* Create an 8-bit palettized surface and render the given text at
    fast quality with the given font and color.  The 0 pixel is the
