59967dea0a4ef55b9c466bca95976f60b2d556dc
Ozkan Sezer <sezeroz@gmail.com>
2021-07-12 01:22:50 +0300

    fixed the custom harfbuzz config.h for unix case

--- external/harfbuzz-2.8.0/config.h
+++ external/harfbuzz-2.8.0/config.h
@@ -2,7 +2,7 @@
 #  include "config_sdl_win32.h"
 
 #elif defined(__APPLE__) || defined(__ANDROID__) || defined(__unix__)
-#  include "config_sdl_android.h"
+#  include "config_sdl_unix.h"
 
 #else
 #error generate a harfbuzz config for your platform
