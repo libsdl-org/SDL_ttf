b31ee728430e71f1f6e927f89284af9fd58bd1d9
Sam Lantinga <slouken@libsdl.org>
2022-07-06 07:49:30 -0700

    Updated version to 2.20.0 for release

--- SDL_ttf.h
+++ SDL_ttf.h
@@ -48,8 +48,8 @@ extern "C" {
  * Printable format: "%d.%d.%d", MAJOR, MINOR, PATCHLEVEL
  */
 #define SDL_TTF_MAJOR_VERSION   2
-#define SDL_TTF_MINOR_VERSION   19
-#define SDL_TTF_PATCHLEVEL      3
+#define SDL_TTF_MINOR_VERSION   20
+#define SDL_TTF_PATCHLEVEL      0
 
 /**
  * This macro can be used to fill a version structure with the compile-time
