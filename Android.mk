fc15c8a5b4092d9880d056e3be150e5562315401
Sam Lantinga <slouken@libsdl.org>
2025-01-29 18:39:55 -0800

    Fixed Android build

--- Android.mk
+++ Android.mk
@@ -26,6 +26,7 @@ LOCAL_MODULE := SDL3_ttf
 LOCAL_SRC_FILES := \
 	src/SDL_ttf.c.neon \
 	src/SDL_hashtable.c \
+	src/SDL_hashtable_ttf.c \
 	src/SDL_gpu_textengine.c \
 	src/SDL_renderer_textengine.c \
 	src/SDL_surface_textengine.c
