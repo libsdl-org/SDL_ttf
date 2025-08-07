aa203b7f33a76a5e6519134bbccc2fb95d4facd9
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-08-07 14:44:14 +0200

    android: support Google Play 16 kiB page size requirement

--- Android.mk
+++ Android.mk
@@ -75,6 +75,10 @@ LOCAL_SHARED_LIBRARIES := SDL3
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/include
 
+# https://developer.android.com/guide/practices/page-sizes
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+LOCAL_LDFLAGS += "-Wl,-z,common-page-size=16384"
+
 include $(BUILD_SHARED_LIBRARY)
 
 ###########################
