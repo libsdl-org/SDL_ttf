a42434b8c96daaf7650dbd0befe480c090d1c2eb
Ivan Vecera <ivan@cera.cz>
2026-06-18 09:35:42 +0200

    Update solid fragment shader for procedural edge anti-aliasing
    Add tex_coord input and use screen-space derivative-based distance
    computation to produce smooth anti-aliased edges on solid fill
    rectangles (underline and strikethrough). Recompile shader binaries
    for SPIR-V, MSL and DXIL.

--- examples/testgputext/shaders/shader-solid.frag.hlsl
+++ examples/testgputext/shaders/shader-solid.frag.hlsl
@@ -1,5 +1,6 @@
 struct PSInput {
     float4 color : TEXCOORD0;
+    float2 tex_coord : TEXCOORD1;
 };
 
 struct PSOutput {
@@ -8,6 +9,8 @@ struct PSOutput {
 
 PSOutput main(PSInput input) {
     PSOutput output;
-    output.color = input.color;
+    float2 dd = (0.5 - abs(input.tex_coord - 0.5)) / fwidth(input.tex_coord);
+    float aa = clamp(min(dd.x, dd.y), 0.0, 1.0);
+    output.color = float4(input.color.rgb, input.color.a * aa);
     return output;
 }
